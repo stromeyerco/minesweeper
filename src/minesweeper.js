096a9c080b92a1b29fd0401f8ee84fad4336d071
stromeyerco <cs@springup.uk>
2017-08-09 09:10:16 +0100

    create project - step 2 + comments

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,17 +1,22 @@
+// create hard coded blank line for board
 const blankLine = "  |   | ";
 
+// text above blank board
 console.log("This is what an empty board would look like:");
 
+// display blank board
 console.log(blankLine);
 console.log(blankLine);
 console.log(blankLine);
 
+// add hard coded guess + bomb lines to board
 const guessLine = "1 |   | ";
 const bombLine = "  | B | ";
 
-
+// text above board with 3 variants
 console.log("This is what a board with a guess and a bomb on it would look like:");
 
+// display board with guess, bomb + blank line
 console.log(guessLine);
 console.log(bombLine);
 console.log(blankLine);
