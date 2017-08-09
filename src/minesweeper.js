8dedfb44aaf192219420a1575381165b395ca89e
stromeyerco <cs@springup.uk>
2017-08-09 09:06:28 +0100

    create project - step 2

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,4 +1,4 @@
-const blankLine = "  |   |  ";
+const blankLine = "  |   | ";
 
 console.log("This is what an empty board would look like:");
 
@@ -6,5 +6,12 @@ console.log(blankLine);
 console.log(blankLine);
 console.log(blankLine);
 
+const guessLine = "1 |   | ";
+const bombLine = "  | B | ";
 
 
+console.log("This is what a board with a guess and a bomb on it would look like:");
+
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
